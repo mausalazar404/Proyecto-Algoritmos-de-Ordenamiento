f28759e323389eef0e58116bc2e769860ae603d0
mausalazar404 <a01712976@tec.mx>
2025-11-18 11:33:43 -0600

    Add complexity comments to merge sort functions
    Added complexity comments for merge sort functions.

--- sorts.h
+++ sorts.h
@@ -3,7 +3,10 @@
 
 #include "dlistVuelo.h"
 
+// Complejidad: O(n log n) 
 void mergeSortPrecio(DListVuelo &lista);
+
+// Complejidad: O(n log n)      
 void mergeSortDestino(DListVuelo &lista);
 
 #endif
