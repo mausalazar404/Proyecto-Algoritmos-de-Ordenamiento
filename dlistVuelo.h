8481ee291020791f096852e5c957004ca9b01a40
mausalazar404 <a01712976@tec.mx>
2025-11-18 11:33:15 -0600

    Update dlistVuelo.h

--- dlistVuelo.h
+++ dlistVuelo.h
@@ -11,32 +11,58 @@ public:
     DLinkVuelo* next;
     DLinkVuelo* prev;
 
+    // Complejidad: O(1) 
     DLinkVuelo(Vuelo v);
 };
 
 class DListVuelo {
 public:
+    // Complejidad: O(1) 
     DListVuelo();
 
 
+    // Complejidad: O(1)
     void addFirst(Vuelo v);
+    
+    // Complejidad: O(1) 
     void add(Vuelo v);
+    
+    // Complejidad: O(1) 
     Vuelo getFirst();
+    
+    // Complejidad: O(1)
     Vuelo removeFirst();
+    
+    // Complejidad: O(1) 
     Vuelo getLast() ;
+    
+    // Complejidad: O(1) 
     Vuelo removeLast();
+    
+    // Complejidad: O(1) 
     int length();
+    
+    // Complejidad: O(1) 
     bool empty();
+    
+    // Complejidad: O(n) 
     void clear();
+    
+    // Complejidad: O(n) 
     void mostrar();
     
+    // Complejidad: O(1)
     DLinkVuelo* getHead(){ 
         return head;
     }
+    
+    // Complejidad: O(1) - Asignación a 'head'.
     void setHead(DLinkVuelo* newHead) { 
         head = newHead; 
     }
-    void updateTail(); 
+    
+    // Complejidad: O(n) 
+    void updateTail();
 
 private:
     DLinkVuelo* head;
