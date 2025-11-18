76b1485953e555751e8dcc274a837616c66e175e
mausalazar404 <a01712976@tec.mx>
2025-11-18 11:32:37 -0600

    Update vuelo.h

--- vuelo.h
+++ vuelo.h
@@ -2,7 +2,7 @@
 #define VUELO_H
 
 #include <string>
-using namespace std;
+using namespace std;    
 
 class Vuelo {
 private:
@@ -12,11 +12,22 @@ private:
     double precio;
 
 public:
+    // Complejidad: O(1) 
     Vuelo(string num, string dest, string hora, double prec);
+    
+    // Complejidad: O(1)
     string getDestino();
+    
+    // Complejidad: O(1) 
     string getHora();
+    
+    // Complejidad: O(1) 
     string getNumero();
+    
+    // Complejidad: O(1) 
     double getPrecio();
+    
+    // Complejidad: O(1) 
     void mostrar();
 };
 
