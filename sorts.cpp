9f1b6fb5f7232c133115e73aa9bdec3b00c6ecd3
mausalazar404 <a01712976@tec.mx>
2025-11-18 11:34:01 -0600

    Update sorts.cpp

--- sorts.cpp
+++ sorts.cpp
@@ -2,51 +2,58 @@
 #include <iostream>
 #include <string>
 
-
+// Complejidad: O(n)
 DLinkVuelo* sortedMergePrecio_simple(DLinkVuelo* a, DLinkVuelo* b);
+
+// Complejidad: O(n)
 DLinkVuelo* sortedMergeDestino_simple(DLinkVuelo* a, DLinkVuelo* b);
+
+// Complejidad: O(n)
 void splitList(DLinkVuelo* head, DLinkVuelo** frontRef, DLinkVuelo** backRef);
+
+// Complejidad: O(n log n)
 DLinkVuelo* mergeSortRecursive(DLinkVuelo* head, bool sortByPrecio);
 
-
+// Complejidad: O(n log n)
 void mergeSortPrecio(DListVuelo& lista) {
     if (lista.empty()) return;
 
     DLinkVuelo* newHead = mergeSortRecursive(lista.getHead(), true);
     lista.setHead(newHead);
-    lista.updateTail(); 
+    lista.updateTail(); // O(n)
     std::cout << "\nLista ordenada por Precio." << std::endl;
 }
 
+// Complejidad: O(n log n)
 void mergeSortDestino(DListVuelo& lista) {
     if (lista.empty()) return;
 
     DLinkVuelo* newHead = mergeSortRecursive(lista.getHead(), false);
     lista.setHead(newHead);
-    lista.updateTail(); 
+    lista.updateTail(); // O(n)
     std::cout << "Lista ordenada por Destino." << std::endl;
 }
 
-
+// Complejidad: O(n log n)
 DLinkVuelo* mergeSortRecursive(DLinkVuelo* head, bool sortByPrecio) {
     if (!head || !head->next) {
         return head;
     }
 
     DLinkVuelo *a, *b;
-    splitList(head, &a, &b);
+    splitList(head, &a, &b); // O(n)
 
-    a = mergeSortRecursive(a, sortByPrecio);
-    b = mergeSortRecursive(b, sortByPrecio);
+    a = mergeSortRecursive(a, sortByPrecio); 
+    b = mergeSortRecursive(b, sortByPrecio); 
 
     if (sortByPrecio) {
-        return sortedMergePrecio_simple(a, b);
+        return sortedMergePrecio_simple(a, b); // O(n)
     } else {
-        return sortedMergeDestino_simple(a, b);
+        return sortedMergeDestino_simple(a, b); // O(n)
     }
 }
 
-
+// Complejidad: O(n)
 void splitList(DLinkVuelo* head, DLinkVuelo** frontRef, DLinkVuelo** backRef) {
     DLinkVuelo* fast;
     DLinkVuelo* slow;
@@ -60,6 +67,7 @@ void splitList(DLinkVuelo* head, DLinkVuelo** frontRef, DLinkVuelo** backRef) {
     slow = head;
     fast = head->next;
 
+    
     while (fast != nullptr) {
         fast = fast->next;
         if (fast != nullptr) {
@@ -73,7 +81,7 @@ void splitList(DLinkVuelo* head, DLinkVuelo** frontRef, DLinkVuelo** backRef) {
     slow->next = nullptr; 
 }
 
-
+// Complejidad: O(n)
 DLinkVuelo* sortedMergePrecio_simple(DLinkVuelo* a, DLinkVuelo* b) {
     DLinkVuelo* result = nullptr;
 
@@ -88,10 +96,11 @@ DLinkVuelo* sortedMergePrecio_simple(DLinkVuelo* a, DLinkVuelo* b) {
         result->next = sortedMergePrecio_simple(a, b->next);
     }
     
+
     return result;
 }
 
-
+// Complejidad: O(n)
 DLinkVuelo* sortedMergeDestino_simple(DLinkVuelo* a, DLinkVuelo* b) {
     DLinkVuelo* result = nullptr;
 
